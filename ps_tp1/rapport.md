50dceae3ce7f9e061bc389fbd9534a535a52bd1c
JacksonKnew <paullestrat@hotmail.fr>
2023-10-09 15:33:47 +0200

    1.2

--- ps_tp1/rapport.md
+++ ps_tp1/rapport.md
@@ -8,3 +8,4 @@ On remarque que le PGRP est le même que le PID, il y a donc par défaut un grou
 
 ### 1.2
 
+La fonction stop_handler est exécutée et remplace le comportement par défaut si le bon signal est envoyé (ici SIGINT au début ou SIGTERM si kill est utilisé sans rien). kill -9 en revanche envoie un signal qui ne peut pas être utilisé et qui interromp donc toujours le processus.
--- ps_tp1/server_2.c
+++ ps_tp1/server_2.c
@@ -5,3 +5,46 @@
  * server_2.c
  */
 
+#define _XOPEN_SOURCE 700
+
+// for printf()
+#include <stdio.h>
+// For rand(), srand(), sleep(), EXIT_SUCCESS
+#include <stdlib.h>
+// For time()
+#include <time.h>
+// For getpid(), getppid()
+#include <unistd.h>
+#include <signal.h>
+
+#include <stdbool.h>
+
+volatile bool running = true;
+
+void stop_handler(int sig) {
+    printf("%d\n", sig);
+    running = false;
+}
+
+int main()
+{
+    printf("Hello there!\n");
+    srand(999);
+
+    struct sigaction sig;   
+    sig.sa_handler = &stop_handler;
+    sig.sa_flags = 0;
+
+    sigaction(SIGINT, &sig, NULL);   
+    sigaction(SIGTERM, &sig, NULL); 
+
+    while (running) {
+        printf("pid : %d | ppid : %d | grp : %d\n", getpid(), getppid(), getpgrp());
+        printf("%d\n", rand());
+        sleep(1);
+    }
+
+    printf("Loop exited\n");
+    return EXIT_SUCCESS;
+}
+
