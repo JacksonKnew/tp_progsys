ab1a8a9cac215402bbcd63dd3a826b4a0db77666
JacksonKnew <paullestrat@hotmail.fr>
2023-10-09 16:38:13 +0200

    2.1

--- ps_tp1/rapport.md
+++ ps_tp1/rapport.md
@@ -14,3 +14,18 @@ La fonction stop_handler est exécutée et remplace le comportement par défaut 
 
 atexit est activée peut importe le signal sauf avec kill -9
 
+## 2
+
+### 2.1
+
+tous les processus d'un même groupe s'arrêtent en même temps car le terminal envoie le signal à tous les processus du groupe actif de la session. Selon la doc:
+
+> If a signal is generated from the
+       terminal (e.g., typing the interrupt key to generate SIGINT),
+       that signal is sent to the foreground process group.
+
+le processus fils est zombifié lorsqu'il est kill jusqu'a ce que le processus père soit kill.
+
+Lorsqu'on tue le père le fils est adopté par le grand père.
+
+### 2.2
--- ps_tp1/server_3.c
+++ ps_tp1/server_3.c
@@ -5,3 +5,61 @@
  * server_3.c
  */
 
+
+#define _XOPEN_SOURCE 700
+
+// for printf()
+#include <stdio.h>
+// For rand(), srand(), sleep(), EXIT_SUCCESS
+#include <stdlib.h>
+// For time()
+#include <time.h>
+// For getpid(), getppid()
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <stdbool.h>
+
+volatile bool running = true;
+char is_parent[256];
+pid_t cpid;
+
+void stop_handler(int sig) {
+    printf("%s || %d\n", is_parent, sig);
+    running = false;
+    if (cpid) kill(cpid, SIGTERM);
+}
+
+void exit_message() {
+    printf("Process %s killed\n", is_parent);
+}
+
+int main()
+{
+
+    cpid = fork();
+
+    strncpy(is_parent, cpid ? "child " : "parent", 256);
+
+    printf("Hello there %s!\n", is_parent);
+    srand(999);
+
+    struct sigaction sig;   
+    sig.sa_handler = &stop_handler;
+    sig.sa_flags = 0;
+
+    sigaction(SIGINT, &sig, NULL);   
+    sigaction(SIGTERM, &sig, NULL); 
+    sigaction(SIGCHLD, &sig, NULL);
+
+    atexit(exit_message);
+
+    while (running) {
+        printf("%s || pid : %d | ppid : %d | grp : %d | %d\n", is_parent, getpid(), getppid(), getpgrp(), rand());
+        sleep(1);
+    }
+
+    printf("Loop exited\n");
+    return EXIT_SUCCESS;
+}
+
