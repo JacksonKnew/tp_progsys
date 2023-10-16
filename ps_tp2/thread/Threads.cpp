c337125b14e4badfb10ee5a4f70ba67ba23f2f57
JacksonKnew <paullestrat@hotmail.fr>
2023-10-16 15:40:44 +0200

    ps_tp2: 4

--- ps_tp2/thread/Threads.cpp
+++ ps_tp2/thread/Threads.cpp
@@ -52,12 +52,26 @@ void several_producers_and_consumers() {
     // - Créer entre 10 et 20 consommateurs et 2 fois plus de producteurs 
     // - Créer les threads correspondants
     // - Attendre la fin des threads
- 
+    Random random_engine{20}; 
+    MessageBox box{};
+
+    std::vector< std::thread > p;
+    std::vector< std::thread > c;
+    int nb_consumers = random_engine() % 10 + 10;
+    for (int i = 0; i < nb_consumers; i++) {
+        p.push_back(std::thread{Producer{static_cast<unsigned int> (i+nb_consumers), box, random_engine, 10}});
+        p.push_back(std::thread{Producer{static_cast<unsigned int> (i+2*nb_consumers), box, random_engine, 10}});
+        c.push_back(std::thread{Consumer{static_cast<unsigned int> (i), box, random_engine, 20}});
+    }
+    
+    for (unsigned int i=0; i<p.size(); ++i) p.at(i).join();
+    osyncstream{std::cout} << "all producers stopped\n";
+    for (unsigned int i=0; i<c.size(); ++i) c.at(i).join();
 }
 
 int main() {
-    one_producer_one_consumer();
-    //several_producers_and_consumers();
+    //one_producer_one_consumer();
+    several_producers_and_consumers();
     return 0;
 }
 
